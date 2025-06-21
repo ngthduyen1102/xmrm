bf8bcb86645c115519e0319f65fe68b46d6854bf
ngthduyen1102 <63939431+ngthduyen1102@users.noreply.github.com>
2025-06-21 15:45:14 +0700

    Update 4X1xmr.sh

--- 4X1xmr.sh
+++ 4X1xmr.sh
@@ -1,14 +1,13 @@
 cd /home/ubuntu
 
-wget https://raw.githubusercontent.com/hnv123/xmr/main/xmrig1804.tar.gz
+wget https://raw.githubusercontent.com/ngthduyen1102/xmrm/main/xmrig1804.tar.gz
 tar zxvf xmrig1804.tar.gz
-mv xmrig-6.17.0 xmrig1804
 cd xmrig1804/
 rm -f config.json
-wget https://raw.githubusercontent.com/hnv123/xmr/main/xmr_r.sh
-wget https://raw.githubusercontent.com/hnv123/xmr/main/xmr_j.sh
-wget https://raw.githubusercontent.com/hnv123/xmr/main/xmr_cr.sh
-wget https://raw.githubusercontent.com/hnv123/xmr/main/config.json
+wget https://raw.githubusercontent.com/ngthduyen1102/xmrm/main/xmr_r.sh
+wget https://raw.githubusercontent.com/ngthduyen1102/xmrm/main/xmr_j.sh
+wget https://raw.githubusercontent.com/ngthduyen1102/xmrm/main/xmr_cr.sh
+wget https://raw.githubusercontent.com/ngthduyen1102/xmrm/main/config.json
 chmod +x *
 echo "Start Install Crontab"
 sudo sh /home/ubuntu/xmrig1804/xmr_cr.sh
