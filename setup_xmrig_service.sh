d8e5e8ed08eda041247ae00e1b22ae3c78071108
ngthduyen1102 <63939431+ngthduyen1102@users.noreply.github.com>
2025-06-21 16:56:07 +0700

    Create setup_xmrig_service.sh

---
+++ setup_xmrig_service.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Đường dẫn tuyệt đối tới thư mục build
+WORKDIR="/home/user/xmrig/build"
+
+# Tạo file service
+sudo bash -c "cat > /etc/systemd/system/xmrig.service <<EOF
+[Unit]
+Description=XMRig Miner
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+WorkingDirectory=${WORKDIR}
+ExecStartPre=/bin/sleep 20
+ExecStart=${WORKDIR}/xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.cogang -p x
+Restart=always
+Nice=10
+StandardOutput=append:${WORKDIR}/xmrig.log
+StandardError=append:${WORKDIR}/xmrig.err
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+# Reload systemd
+sudo systemctl daemon-reload
+
+# Enable service
+sudo systemctl enable xmrig
+
+# Start service
+sudo systemctl start xmrig
+
+# Kiểm tra trạng thái
+sudo systemctl status xmrig
